81a8438f7ec0ad8372760153cb18cbe1983d2af8
Ahsan Abid <139261566+ahsan-it@users.noreply.github.com>
2026-05-17 16:25:33 +0500

    Add files via upload
    The objective of this project is to develop a basic Gym Management System website using Flask, HTML, and CSS. The website provides information about the gym, membership plans, and gym facilities.
    Technologies Used
    Python
    Flask
    HTML
    CSS

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home Route
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# About Route
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Membership Route
+@app.route('/membership')
+def membership():
+    return render_template('membership.html')
+
+# Run Application
+if __name__ == '__main__':
+    app.run(debug=True)
